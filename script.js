4701d2bb2fdb2bf6ed6aa9c591aacd51b23603d9
BoyIsSalsa <151720961+BoyIsSalsa@users.noreply.github.com>
2023-11-22 00:50:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let totalVotes = 0;
 
+function vote(game) {
+    // Simulate backend logic to store votes
+    totalVotes++;
+    
+    // Update the frontend to reflect the new total
+    document.getElementById('result').innerText = `Total Votes: ${totalVotes}`;
+}
